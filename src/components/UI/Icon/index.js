d324cb40675a2797409eee86debb4a42f3ea7b13
md5dalton <md5daltonggs@gmail.com>
2022-09-25 22:11:34 -0400

    Update index.js

--- src/components/UI/Icon/index.js
+++ src/components/UI/Icon/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import Img from "../Img"
 
-export default ({ name }) => <Img className="image-icon" name={`icons/icon-${name}.svg`} />
+export default ({ name }) => <Img className="image-icon" src={`icons/icon-${name}.svg`} />
