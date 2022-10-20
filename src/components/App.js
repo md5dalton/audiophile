d9951f5f15b34fec4591b81a5a0b559017faccfb
md5dalton <md5daltonggs@gmail.com>
2022-10-20 12:27:37 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import "./App.sass"
+import Category from './Routes/Category'
 import Home from './Routes/Home'
 
 const App = () => (
     <Router>
-        <Home />
+        {/* <Home /> */}
+        <Category />
     </Router>
 )
 
