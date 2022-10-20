02bf1b3ec57224b32971107539a60c4f47816e50
md5dalton <md5daltonggs@gmail.com>
2022-10-20 10:52:04 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import Layout from './Layout'
 
 import "./App.sass"
 import Home from './Routes/Home'
 
 const App = () => (
     <Router>
-        <Layout>
-            <Home />
-        </Layout>
+        <Home />
     </Router>
 )
 
