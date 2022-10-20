59f81839f76b64070164caa0e741fd0b874b8867
md5dalton <md5daltonggs@gmail.com>
2022-10-20 12:28:04 -0400

    Update index.js

--- src/components/Routes/Category/index.js
+++ src/components/Routes/Category/index.js
@@ -1,89 +1,18 @@
 import React from "react"
-import Button from "../../UI/Button"
-import List from "../../UI/List"
-import { Link } from "react-router-dom"
-import Icon from "../../UI/Icon"
-import Picture from "../../UI/Picture"
+import Page from "../../Layout/Page"
+import CategoriesLinks from "../../Layout/CategoriesLinks"
 
 import "./styles.sass"
-import Page from "../../Layout/Page"
-
-const categories = [
-    "headphones", 
-    "speakers", 
-    "earphones"
-]
+import About from "../../Layout/About"
 
 export default () => {
 
-    const getResponsiveImages = (prepend, image) => ({
-        mobile: `${prepend}/mobile/${image}`,
-        tablet: `${prepend}/tablet/${image}`,
-        desktop: `${prepend}/desktop/${image}`
-    })
-
-    const categoryHandler = category => (
-        <li key={category} className="category">
-            <img src={`./assets/shared/desktop/image-category-thumbnail-${category}.png`} alt="" />
-            <div className="name">{category}</div>
-            <Link to={`product/${category}`}>
-                <span>shop</span>
-                <Icon name="arrow-right" />
-            </Link>
-        </li>
-    )
-
-    const images = [
-        "speaker-zx9.png", 
-        "speaker-zx7.jpg", 
-        "earphones-yx1.jpg"
-    ].map(image => getResponsiveImages("./assets/home", `image-${image}`))
-
-    const header = (
-        <section className="hero">
-            <overline>New product</overline>
-            <h1>XX99 Mark II Headphones</h1>
-            <p>
-                Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.
-            </p>
-            <Button>See product</Button>
-        </section>
-    )
+    const header = <h4>headphones</h4>
     
     return (
-        <Page name="home" header={header}>
-            <section className="categories">
-                <List items={categories} itemHandler={categoryHandler} />
-            </section>
-            <section className="products">
-                <div className="product primary">
-                    <Picture {...images[0]} />
-                    <h1>ZX9 speaker</h1>
-                    <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                    <Button>See product</Button>
-                </div>
-                <div className="product secondary colapsed">
-                    <Picture {...images[1]} />
-                    <h4>ZX7 speaker</h4>
-                    <Button>See product</Button>
-                </div>
-                <div className="product secondary dettached">
-                    <Picture {...images[2]} />
-                    <h4>YX1 earphones</h4>
-                    <Button>See product</Button>
-                </div>
-            </section>
-            <section className="about">
-                <Picture {...getResponsiveImages("./assets/shared", "image-best-gear.jpg")} />
-                <h4>Bringing you the <span>best</span> audio gear</h4>
-                <p>
-                    Located at the heart of New York City, Audiophile is the premier store for high end headphones, 
-                    earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration 
-                    rooms available for you to browse and experience a wide range of our products. Stop by our store 
-                    to meet some of the fantastic people who make Audiophile the best place to buy your portable 
-                    audio equipment.
-                </p>
-            </section>
+        <Page name="category headphones" header={header}>
+            <CategoriesLinks />
+            <About />
         </Page>
     )
 }
