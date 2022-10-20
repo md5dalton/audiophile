74b4dd4c113b9cd1096c94cb74e3c1373e87f603
md5dalton <md5daltonggs@gmail.com>
2022-10-20 11:25:29 -0400

    Create index.js

---
+++ src/components/Layout/About/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Picture from "../../UI/Picture"
+import { getResponsiveImages } from "../../../Functions"
+
+import "./styles.sass"
+
+export default () => (
+    <section className="about">
+        <Picture {...getResponsiveImages("./assets/shared", "image-best-gear.jpg")} />
+        <h4>Bringing you the <span>best</span> audio gear</h4>
+        <p>
+            Located at the heart of New York City, Audiophile is the premier store for high end headphones, 
+            earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration 
+            rooms available for you to browse and experience a wide range of our products. Stop by our store 
+            to meet some of the fantastic people who make Audiophile the best place to buy your portable 
+            audio equipment.
+        </p>
+    </section>
+)
