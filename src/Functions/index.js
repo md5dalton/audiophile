f13012bdfd5b47ac7089b43f49872393df3563c9
md5dalton <md5daltonggs@gmail.com>
2022-10-23 15:08:22 +0200

    Update index.js

--- src/Functions/index.js
+++ src/Functions/index.js
@@ -1,5 +1,10 @@
+import data from "../assets/data.json"
+
 export const getResponsiveImages = (prepend, image) => ({
     mobile: `${prepend}/mobile/${image}`,
     tablet: `${prepend}/tablet/${image}`,
     desktop: `${prepend}/desktop/${image}`
 })
+
+export const getCategoryItems = category => data.filter(item => item.category === category)
+export const getProduct = slug => data.find(item => item.slug === slug)
