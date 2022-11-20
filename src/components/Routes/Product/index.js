bdb2d8dbad23c7bb48b72bc13f238cf0c2797193
md5dalton <md5daltonggs@gmail.com>
2022-11-20 12:19:52 +0200

    Update index.js

--- src/components/Routes/Product/index.js
+++ src/components/Routes/Product/index.js
@@ -1,36 +1,45 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom"
 import List from "../../UI/List"
 import Page from "../../Layout/Page"
 import About from "../../Layout/About"
 import Picture from "../../UI/Picture"
-import useData from "../../../hooks/useData"
 import CategoriesLinks from "../../Layout/CategoriesLinks"
 import { getProduct } from "../../../Functions"
+import Button from "../../UI/Button"
 
 import "./styles.sass"
-import Button from "../../UI/Button"
 
 export default () => {
 
+    const [ searchParams, setSearchParams ] = useSearchParams()
+
+    const product = getProduct(searchParams.get("s"))
+
     const {
         image, new: isNew, name, description, slug, price, features, includes, gallery, others
-    } = getProduct("xx99-mark-two-headphones")
+    } = product
+    
+    console.log(product)
 
     return (
         <Page name="product">
             <section className="product-detail">
-                <Link className="go-back">go back</Link>
                 <div className="product-item">
+                    <Link className="go-back">go back</Link>
                     <Picture {...image} />
                     <div className="details">
                         {!isNew && <overline className="new-product">new product</overline>}
                         <h4>{name}</h4>
                         <p>{description}</p>
-                        <h6 className="price">${Intl.NumberFormat("en-US").format(price)}</h6>
+                        <h6 className="price">$ {Intl.NumberFormat("en-US").format(price)}</h6>
                         <form>
-                            <input type="number" defaultValue={1} />
-                            <Button>add to cart</Button>
+                            <div className="quantity-input">
+                                <Button>-</Button>
+                                <input type="number" defaultValue={1} />
+                                <Button>+</Button>
+                            </div>
+                            <Button className="primary-button">add to cart</Button>
                         </form>
                     </div>
                 </div>
@@ -44,31 +53,31 @@ export default () => {
                         items={includes} 
                         itemHandler={({quantity, item}, index) => (
                             <li key={index}>
-                                <div>{quantity}x</div> 
-                                <div>{item}</div> 
+                                <span className="quantity-counter">{quantity}x</span> 
+                                <p>{item}</p> 
                             </li>
                         )}
                     />
                 </div>
-                <div className="includes">
-                    <List 
-                        items={Object.values(gallery)} 
-                        itemHandler={(images, index) => <li key={index}><Picture {...images} /></li> }
-                    />
-                </div>
-                <div className="related">
-                    <h5>you may also like</h5>
-                    <List 
-                        items={others} 
-                        itemHandler={({ image, slug, name }, index) => (
-                            <li key={index}>
-                                <Picture {...image} />
-                                <h5>{name}</h5>
-                                <Link to={slug} className="link-button">see product</Link>
-                            </li>
-                        )}
-                    />
-                </div>
+            </section>
+            <section className="gallery">
+                <List 
+                    items={Object.values(gallery)} 
+                    itemHandler={(images, index) => <li key={index}><Picture {...images} /></li> }
+                />
+            </section>
+            <section className="related">
+                <h5>you may also like</h5>
+                <List 
+                    items={others} 
+                    itemHandler={({ image, slug, name }, index) => (
+                        <li key={index}>
+                            <Picture {...image} />
+                            <h5>{name}</h5>
+                            <Link to={`/p?s=${slug}`} className="link-button">see product</Link>
+                        </li>
+                    )}
+                />
             </section>
             <CategoriesLinks />
             <About />
