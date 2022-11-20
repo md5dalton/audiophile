b63b0a89e53a84691bd09f356d4f3f843d4a6231
md5dalton <md5daltonggs@gmail.com>
2022-11-20 16:08:50 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import "./App.sass"
 import Category from './Routes/Category'
 import Home from './Routes/Home'
 import Product from './Routes/Product'
+
+import "./App.sass"
+import Checkout from './Routes/Checkout'
 
 const App = () => (
     <Router>
@@ -13,6 +15,7 @@ const App = () => (
             <Route path="/" element={<Home />} />
             <Route path="/c/*" element={<Category />} />
             <Route path="/p/*" element={<Product />} />
+            <Route path="/checkout" element={<Checkout />} />
         </Routes>
     </Router>
 )
