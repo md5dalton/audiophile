d0c707945bf8a305c7ef1ebfce16d3d82bee0e8a
md5dalton <md5daltonggs@gmail.com>
2022-12-16 18:55:27 +0200

    Update CartContext.js

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -5,25 +5,40 @@ const CartContext = createContext()
 
 export function CartProvider ({ children }) {
 
-    const dumy = [
-        {
-            slug: "xx99-mark-two-headphones",
-            quantity: 6
-        },
-        {
-            slug: "zx7-speaker",
-            quantity: 2
-        },
-        {
-            slug: "yx1-earphones",
-            quantity: 1
-        }
-    ]
+    const [ cart, setCart ] = useState([])
 
-    const [ products, setProducts ] = useState(dumy.map(({ slug, quantity }) => ({ ...getProduct(slug), quantity: quantity })))
-    
+    const clearCart = () => setCart([])
+
+    const addCart = (slug, quantity) => {
+        
+        let product = cart.find(item => item.slug === slug)
+
+        setCart(
+            (product ? 
+            cart.map(item => slug === item.slug ? {...item, quantity: quantity} : item) :
+            [...cart, {slug, quantity}]).filter(item => item.quantity)
+        )
+        
+    }
+
+    const getTotalPrice = () => cart.reduce((a, { slug, quantity }) => {
+
+        const { price } = getProduct(slug)
+
+        return a + price * quantity
+
+    })
+
+
     return (
-        <CartContext.Provider value={{ products }}>
+        <CartContext.Provider 
+            value={{ 
+                cart,
+                addCart,
+                clearCart,
+                getTotalPrice
+            }}
+        >
             {children}
         </CartContext.Provider>
     )
