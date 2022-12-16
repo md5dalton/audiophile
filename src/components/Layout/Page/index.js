e9687247510b40763216c0d841d65fddc82fbb5f
md5dalton <md5daltonggs@gmail.com>
2022-12-16 12:50:10 +0200

    Update index.js

--- src/components/Layout/Page/index.js
+++ src/components/Layout/Page/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react"
+import { CartProvider } from "../../../context/CartContext"
 import Footer from "../Footer"
 import Header from "../Header"
 import Cart from "../Modals/Cart"
@@ -9,19 +10,25 @@ import "./styles.sass"
 export default ({ name, header, children }) => {
 
     const [ sidedrawer, setSidedrawer ] = useState(false)
+    const [ cart, setCart ] = useState(false)
 
     const toggleSidedrawer = () => setSidedrawer(!sidedrawer)
+    const toggleCart = () => setCart(!cart)
     
     return (
         <Fragment>
-            <Header className={name} sidedrawerToggleHandler={toggleSidedrawer} >
+            <Header 
+                className={name} 
+                sidedrawerToggleHandler={toggleSidedrawer}
+                cartToggleHandler={toggleCart}
+            >
                 {header}
             </Header>
             <main className={`page ${name}`}>
                 {children}
             </main>
             <aside>
-                {/* <Cart /> */}
+                <Cart isOpen={cart} toggleHandler={toggleCart} />
                 <SideDrawer isOpen={sidedrawer} toggleHandler={toggleSidedrawer} />
             </aside>
             <Footer />
