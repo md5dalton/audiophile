32650c468d780cc78f0b019a416f6894ce8565f4
md5dalton <md5daltonggs@gmail.com>
2022-12-18 19:42:24 +0200

    Update index.js

--- src/components/UI/GoBackLink/index.js
+++ src/components/UI/GoBackLink/index.js
@@ -1,6 +1,18 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import "./styles.sass"
 
-export default () => <Link to="/" className="go-back">go back</Link>
+export default () => {
+
+    const navigate = useNavigate()
+
+    const clickHandler = ev => {
+        ev.preventDefault()
+        navigate(-1)
+    }
+
+    return (
+        <Link to="/" className="button text return-button" onClick={clickHandler}>go back</Link>
+    )
+}
