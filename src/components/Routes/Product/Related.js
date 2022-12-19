2dad9837be82a804350377e3334f39ba7fe48250
md5dalton <md5daltonggs@gmail.com>
2022-12-19 07:35:10 +0200

    Update Related.js

--- src/components/Routes/Product/Related.js
+++ src/components/Routes/Product/Related.js
@@ -5,14 +5,16 @@ import Picture from "../../UI/Picture"
 
 export default ({ others }) => (
     <section className="related">
-        <h5>you may also like</h5>
+        <h5 className="title">you may also like</h5>
         <List 
             items={others} 
             itemHandler={({ image, slug, name }, index) => (
                 <li key={index}>
                     <Picture {...image} />
-                    <h5 className="name">{name}</h5>
-                    <Link to={`/p?s=${slug}`} className="button">see product</Link>
+                    <div className="details">
+                        <h5 className="name">{name}</h5>
+                        <Link to={`/p?s=${slug}`} className="button">see product</Link>
+                    </div>
                 </li>
             )}
         />
