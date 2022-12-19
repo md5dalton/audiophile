11f688968fb1f306f47b38cb622138e399b29c63
md5dalton <md5daltonggs@gmail.com>
2022-12-19 18:08:53 +0200

    Update index.js

--- src/components/UI/ProductRow/index.js
+++ src/components/UI/ProductRow/index.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Picture from "../Picture"
 import { formatPrice } from "../../../Functions"
+import Img from "../Img"
 
 import "./styles.sass"
 
-export default ({ image, name, price, children }) => (
+export default ({ slug, name, price, children }) => (
     <div className="product-row">
-        <Picture {...image} />
+        <Img className="thumbnail" src={`cart/image-${slug}.jpg`} />
         <div className="details">
             <div className="name">{name}</div>
             <div className="price">{formatPrice(price)}</div>
