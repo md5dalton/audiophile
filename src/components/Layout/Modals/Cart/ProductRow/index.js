0f7ca6c359d49fd4b3b4cf9f7b8ad08a03027fa4
md5dalton <md5daltonggs@gmail.com>
2022-12-19 16:43:54 +0200

    Create index.js

---
+++ src/components/Layout/Modals/Cart/ProductRow/index.js
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from "react"
+import ProductRow from "../../../../UI/ProductRow"
+import CounterInput from "../../../../UI/CounterInput"
+import CartContext from "../../../../../context/CartContext"
+
+import "./styles.sass"
+
+export default ({ slug, quantity: pQuantity, ...props}) => {
+    
+    const { addCart } = useContext(CartContext)
+
+    const [ quantity, setQuantity ] = useState(pQuantity)
+
+    const quantityHandler = value => {
+        setQuantity(value)
+        addCart(slug, value)
+    }
+
+    return (
+        <ProductRow {...props}>
+            <CounterInput value={quantity} valueHandler={quantityHandler} />
+        </ProductRow>
+    )
+}
