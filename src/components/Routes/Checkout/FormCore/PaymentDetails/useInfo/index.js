0e2a6c22e5adabc484995f50ac15154cda73c944
md5dalton <md5daltonggs@gmail.com>
2022-12-22 11:14:19 +0200

    Update index.js

--- src/components/Routes/Checkout/FormCore/PaymentDetails/useInfo/index.js
+++ src/components/Routes/Checkout/FormCore/PaymentDetails/useInfo/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Icon from "../../../../../UI/Icon"
+import FieldText from "../../FieldText"
 
 export default id => {
 
     const infoItems = {
-        "e-Money":
+        "Cash on Delivery":
             <div className="cash-on-delivery-info-row">
                 <Icon name="cash-on-delivery" />
                 <p className="description">
@@ -13,26 +14,10 @@ export default id => {
                     so that your order will not be cancelled.
                 </p>
             </div>,
-        "Cash on Delivery":
+        "e-Money":
             <ul role="list" className="fields">
-                <li className="field">
-                    <div className="title small-text">
-                        <label>email</label>
-                        <span className="error-msg">wrong format</span>
-                    </div>
-                    <div className="input-wrapper">
-                        <input type="text" placeholder="mail@mail.com" />
-                    </div>
-                </li>
-                <li className="field">
-                    <div className="title small-text">
-                        <label>email</label>
-                        <span className="error-msg">wrong format</span>
-                    </div>
-                    <div className="input-wrapper">
-                        <input type="text" placeholder="mail@mail.com" />
-                    </div>
-                </li>
+                <FieldText label="e-Money Number" placeholder="59000000" name="e-money-number" />
+                <FieldText label="e-Money PIN" placeholder="1234" name="e-money-pin" type="password" />
             </ul>
         }
 
