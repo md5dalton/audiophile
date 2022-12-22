362d049f365b309e695bb6a5b9bcb89b9e01e5a4
md5dalton <md5daltonggs@gmail.com>
2022-12-22 11:14:41 +0200

    Update index.js

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CartContext from '../../../context/CartContext'
 import Button from '../../UI/Button'
 import Icon from '../../UI/Icon'
 import Nav from '../../UI/Nav'
@@ -6,12 +7,18 @@ import Nav from '../../UI/Nav'
 import "./styles.sass"
 
 export default ({ sidedrawerToggleHandler, cartToggleHandler, children, ...props }) => {
+
+    const { cart } = useContext(CartContext)
+
     return (
         <header {...props}>
             <div className="toolbar">
                 <Button className="menu-toggle" onClick={() => sidedrawerToggleHandler()}><Icon name="hamburger" /></Button>
                 <Nav />
-                <Button onClick={() => cartToggleHandler()}><Icon name="cart" /></Button>
+                <Button className="cart-toggler" onClick={() => cartToggleHandler()}>
+                    <Icon name="cart" />
+                    {cart.length > 0 && <span className="cart-count">{cart.length}</span>}
+                </Button>
             </div>
             <div className="content">
                 {children}
