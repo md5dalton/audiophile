5642beeb46c82aa69f9cb3deeba0855ab69b9761
md5dalton <md5daltonggs@gmail.com>
2022-12-22 11:14:28 +0200

    Update index.js

--- src/components/Layout/Modals/CheckoutModal/index.js
+++ src/components/Layout/Modals/CheckoutModal/index.js
@@ -1,5 +1,6 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import CartContext from "../../../../context/CartContext"
 import { formatPrice } from "../../../../Functions"
 import Icon from "../../../UI/Icon"
 import Modal from "../../../UI/Modal"
@@ -9,6 +10,16 @@ import "./styles.sass"
 
 export default ({ isOpen, toggleHandler, grandTotal }) => {
 
+    const navigate = useNavigate()
+
+    const { clearCart } = useContext(CartContext)
+ 
+    const linkHandler = ev => {
+        ev.preventDefault()
+        clearCart()
+        navigate("/")
+    }
+    
     return (
         <Modal isOpen={isOpen} toggleHandler={toggleHandler}>
             <section>
@@ -25,7 +36,7 @@ export default ({ isOpen, toggleHandler, grandTotal }) => {
                             <div>{formatPrice(grandTotal)}</div>
                         </div>
                     </div>
-                    <Link to="/" className="button back-to-home">back to home</Link>
+                    <Link to="/" className="button back-to-home" onClick={linkHandler}>back to home</Link>
                 </div>
             </section>
         </Modal>
