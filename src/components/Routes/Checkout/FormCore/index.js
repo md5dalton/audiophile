c749a4eca9142f91cd2ba96ed9df1ff64525cabe
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2023-08-22 19:46:31 +0200

    Update index.js

--- src/components/Routes/Checkout/FormCore/index.js
+++ src/components/Routes/Checkout/FormCore/index.js
@@ -13,17 +13,17 @@ export default () => {
             <ul role="list" className="form-inputs fieldsets">
                 <Fieldset legend="billing details">
                     <ul role="list" className="fields">
-                        <FieldText label="Name" placeholder="Majara" name="name" />
+                        <FieldText label="Name" placeholder="Name" name="name" />
                         <FieldText label="Email" placeholder="mail@mail.com" name="email" type="email" />
                         <FieldText label="Phone Number" placeholder="+266 63000000" name="phone" />
                     </ul>
                 </Fieldset>
                 <Fieldset legend="shipping info">
                     <ul role="list" className="fields">
-                        <FieldText label="Address" placeholder="Ha Mpiti, Qacha's Nek" name="address" className="wide" />
-                        <FieldText label="ZIP Code" placeholder="1997" name="zip" />
-                        <FieldText label="City" placeholder="Qacha's Nek" name="city" />
-                        <FieldText label="Country" placeholder="Lesotho" name="country" />
+                        <FieldText label="Address" placeholder="Address" name="address" className="wide" />
+                        <FieldText label="ZIP Code" placeholder="0000" name="zip" />
+                        <FieldText label="City" placeholder="City" name="city" />
+                        <FieldText label="Country" placeholder="Country" name="country" />
                     </ul>
                 </Fieldset>
                 <PaymentDetails />
