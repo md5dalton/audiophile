739b8d844d8d70b7d53d2a0aedff685bab67fde8
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-03-17 14:58:48 +0200

    Update index.js

--- components/UI/Modal/index.js
+++ components/UI/Modal/index.js
@@ -1,12 +1,15 @@
+"use client"
+
+import { useEffect } from "react"
 import Backdrop from "../Backdrop"
 
 import "./styles.sass"
 
 export default ({ isOpen, children, toggleHandler, ...props }) => {
     
-    // useEffect(() => {
-    //     scrollTo(0,0)
-    // }, [isOpen])
+    useEffect(() => {
+        scrollTo(0,0)
+    }, [isOpen])
     
     return isOpen && (
         <>
