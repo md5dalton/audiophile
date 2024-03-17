9dd2eebb194cf8dfcdab383c1fbd04087e11c6e9
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-03-17 15:25:08 +0200

    Update index.js

--- components/Layout/Modals/SideDrawer/index.js
+++ components/Layout/Modals/SideDrawer/index.js
@@ -1,13 +1,19 @@
-import React from "react"
-import Modal from "../../../UI/Modal"
+import { useModal } from "@/context/Modal"
+
+import Modal from "@/components/UI/Modal"
 import CategoriesLinks from "../../CategoriesLinks"
 
 import "./styles.sass"
 
-export default ({ ...props }) => (
-    <Modal {...props}>
-        <div className="side-drawer">
-            <CategoriesLinks />
-        </div>
-    </Modal>
-)
+export default () => {
+    
+    const { toggleSidedrawer, sidedrawer } = useModal()
+
+    return (
+        <Modal toggleHandler={toggleSidedrawer} isOpen={sidedrawer}>
+            <div className="side-drawer">
+                <CategoriesLinks />
+            </div>
+        </Modal>
+    )
+}
