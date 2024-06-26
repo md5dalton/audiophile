8234fab04772b69e2c7005794a62d5826c924006
md5dalton <md5daltonggs@gmail.com>
2024-06-27 01:07:28 +0200

    Update index.js

--- components/Layout/Footer/index.js
+++ components/Layout/Footer/index.js
@@ -1,25 +1,25 @@
-import List from "@/components/UI/List"
-import Icon from "@/components/UI/Icon"
 import Nav from "@/components/UI/Nav"
+import Socials from "./Socials"
 
 import "./styles.sass"
 
 export default () => (
     <footer className="pb-10">
-        <div className="container grid justify-items-center gap-12">
+        <div className="container grid gap-12 justify-items-center md:justify-items-start md:!text-start">
             <div className="accent-bar h-1 w-[100px] bg-brown"></div>
-            <Nav />
-            <p className="description about">
-                Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers 
-                and sound specialists who are devoted to helping you get the most out of personal audio. Come and 
-                visit our demo facility - we’re open 7 days a week.
-            </p>
-            <p className="description copyright">Copyright 2022. All Rights Reserved</p>
-            <List
-                className="socials"
-                items={["facebook", "twitter", "instagram"]}
-                itemHandler={(item, index) => <Icon key={index} name={item} />}
-            />
+            <Nav className="grid gap-y-12 w-full md:justify-items-start lg:flex lg:justify-between" />
+            <div className="lg:grid lg:grid-cols-2">
+                <p>
+                    Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers 
+                    and sound specialists who are devoted to helping you get the most out of personal audio. Come and 
+                    visit our demo facility - we’re open 7 days a week.
+                </p>
+                <div />
+            </div>
+            <div className="grid gap-y-12 justify-items-center w-full md:flex md:justify-between">
+                <p className="font-bold">Copyright 2022. All Rights Reserved</p>
+                <Socials />
+            </div>
         </div>
     </footer>
 )
