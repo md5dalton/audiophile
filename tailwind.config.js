23537c1116603e24c4d626a2592e4955e81047d6
md5dalton <md5daltonggs@gmail.com>
2024-06-26 22:34:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brown": "#D87D4A",
+        "light-brown": "#FBAF85",
+        
+        "light-black": "#101010",
+
+        "dark-grey": "#4C4C4C",
+        "light-grey": "#F1F1F1",
+        "very-light-grey": "#FAFAFA",
+        
+        "border": "#CFCFCF",
+        "error": "#CD2C2C",
+
+      }
+    },
   },
   plugins: [],
 }
