e5a1f7941a166a4f51f8af714a8d4384b1379032
md5dalton <md5daltonggs@gmail.com>
2024-06-28 13:16:28 +0200

    Update index.jsx

--- app/product/[id]/ProductCard/index.jsx
+++ app/product/[id]/ProductCard/index.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { getProduct } from "@/Functions"
-import { addToCart } from "@/signals/cart"
+import { useCart } from "@/context/CartContext"
 
 import ProductCard from "@/components/UI/ProductCard"
 import CounterInput from "@/components/UI/CounterInput"
@@ -11,6 +11,8 @@ import Button from "@/components/UI/Button"
 import "./styles.sass"
 
 export default props => {
+
+    const { addCart } = useCart()
     
     const { slug } = props
 
@@ -21,7 +23,7 @@ export default props => {
     return (
         <ProductCard {...props}>
             <CounterInput value={quantity} valueHandler={setQuantity} />
-            <Button className="button" onClick={() => addToCart(slug, quantity, price)}>add to cart</Button>
+            <Button className="button" onClick={() => addCart(slug, quantity, price)}>add to cart</Button>
         </ProductCard>
     )
 }
