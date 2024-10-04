53c74059ee312b646fe20d1f2e4882e2de04aaae
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-10-04 10:24:38 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,25 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        "brown": "#D87D4A",
-        "light-brown": "#FBAF85",
-        
-        "light-black": "#101010",
+        colors: {
+            "brown": "#D87D4A",
+            "light-brown": "#FBAF85",
+            
+            "light-black": "#101010",
 
-        "grey": "#979797",
-        "dark-grey": "#4C4C4C",
-        "light-grey": "#F1F1F1",
-        "very-light-grey": "#FAFAFA",
-        
-        "border": "#CFCFCF",
-        "error": "#CD2C2C",
-
-      }
+            "grey": "#979797",
+            "dark-grey": "#4C4C4C",
+            "light-grey": "#F1F1F1",
+            "very-light-grey": "#FAFAFA",
+            
+            "border": "#CFCFCF",
+            "error": "#CD2C2C",
+        },
+        backgroundImage: {
+            "header-mobile": "url(/assets/home/mobile/image-header.jpg)",
+            "header-tablet": "url(/assets/home/tablet/image-header.jpg)",
+            "header-desktop": "url(/assets/home/desktop/image-hero.jpg)",
+        }
     },
   },
   plugins: [],
