ff5a4b6c2c04435538a295885b1128ab238fce79
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-10-04 02:03:04 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,5 @@
-"use client"
-
 import { Manrope } from "next/font/google"
-import { ModalProvider } from "@/context/Modal"
-import { CartProvider } from "@/context/CartContext"
+import Provider from "@/context/Provider"
 
 import Footer from "@/components/Layout/Footer"
 import Cart from "@/components/Layout/Modals/Cart"
@@ -15,28 +12,22 @@ const font = Manrope({
     subsets: ["latin"]
 })
 
-// export const metadata = {
-//     title: "",
-//     description: "",
-// }
-
-export default function RootLayout({ children }) {
-    
-    
-    return (
-        <html lang="en" className={`${font.className}`}>
-            <body className="flex flex-col min-h-screen">
-                <CartProvider>
-                <ModalProvider>
-                    {children}
-                    <aside>
-                        <Cart />
-                        <SideDrawer />
-                    </aside>
-                </ModalProvider>
-                </CartProvider>
-                <Footer />
-            </body>
-        </html>
-    )
+export const metadata = {
+    title: "Audiophile",
+    description: "E-commerce website",
 }
+
+export default ({ children }) => (
+    <html lang="en" className={`${font.className}`}>
+        <body className="flex flex-col min-h-screen">
+            <Provider>
+                {children}
+                <aside>
+                    <Cart />
+                    <SideDrawer />
+                </aside>
+            </Provider>
+            <Footer />
+        </body>
+    </html>
+)
