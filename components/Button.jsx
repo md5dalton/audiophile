4f8cee08021396dc9a950f2354d006313e83f465
Ntate Mpiti <90160065+md5dalton@users.noreply.github.com>
2024-10-04 02:14:09 +0200

    Delete Button.jsx

--- components/Button.jsx
+++
@@ -1,9 +0,0 @@
-export default ({ children, className, ...rest }) => (
-    <button
-        className={`
-            ${className}
-            grid place-items-center
-        `.trim()}
-        {...rest}
-    >{children}</button>
-)
